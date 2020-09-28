0568c96eddcfd7034aed79292fc36ec6bced3102
Syed Umair <umair_developer@outlook.com>
2020-09-29 00:49:08 +0530

    Removed Unecessay Create React App Files

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
-
+app.get("*", function (req, res) {
+    res.status(404).send("what???");
+    console.log("This is a 404 Page");
+});
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
