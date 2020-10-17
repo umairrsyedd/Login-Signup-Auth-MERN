d78ffe70556e47063394f96b5fc0d6fb98916b3c
Syed Umair <umair_developer@outlook.com>
2020-10-18 00:23:49 +0530

    Reverted Proxy server to Original API Call

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -5,19 +5,19 @@ import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 // Register User
 export const registerUser = (userData, history) => (dispatch) => {
     axios
-        .post('http://localhost:5000/api/users/register', userData)
+        .post("http://localhost:5000/api/users/register", userData)
         .then((res) => history.push("/login")) // re-direct to login on successful register
         .catch((err) =>
             dispatch({
                 type: GET_ERRORS,
-                payload: err.response.data,
+                payload: err,
             })
         );
 };
 // Login - get user token
 export const loginUser = (userData) => (dispatch) => {
     axios
-        .post('http://localhost:5000/api/users/register', userData)
+        .post("http://localhost:5000/api/users/login", userData)
         .then((res) => {
             // Save to localStorage
             // Set token to localStorage
